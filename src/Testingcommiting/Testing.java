0bf1cdf15b4fb24015bdf8e69e70c63a8a9e35b6
Polina <polya3hus@gmail.com>
2024-01-10 12:22:10 -0600

    I am commiting

--- src/Testingcommiting/Testing.java
+++ src/Testingcommiting/Testing.java
@@ -3,5 +3,6 @@ package Testingcommiting;
 public class Testing {
     public static void main(String[] args) {
         System.out.println("Hi");
+        System.out.println("I just added this");
     }
 }
